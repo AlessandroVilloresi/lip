30dcbd500683be42ce001e404849e3ddec75849f
AlessandroVilloresi <183614593+AlessandroVilloresi@users.noreply.github.com>
2024-11-27 16:37:06 +0100

    contextfree done

--- contextfree/lib/exercises.ml
+++ contextfree/lib/exercises.ml
@@ -16,20 +16,65 @@ let todo : grammar =
 
 
 (* #### Exercise 1, easy (zero_n_one_n) *)
-let zero_n_one_n : grammar = todo
+let zero_n_one_n : grammar =
+  {
+    symbols = [ S ];
+    terminals = [ '0'; '1' ];
+    productions =
+      [              (* Indexes *)
+        S --> "0S1"; (* 0 *)
+        S --> "";    (* 1 *)
+      ];
+    start = S;
+  }
 
 
 (* #### Exercise 2, easy (palindromes) *)
-let palindromes : grammar = todo
+let palindromes : grammar = {
+  symbols = [ S ];
+  terminals = [ '0'; '1' ];
+  productions =
+    [              (* Indexes *)
+      S --> "0S0"; (* 0 *)
+      S --> "1S1"; (* 1 *)
+      S --> "0";   (* 2 *)
+      S --> "1";   (* 3 *)
+      S --> "";    (* 4 *)
+    ];
+  start = S;
+}
 
 
 (* #### Exercise 3, medium (balanced_parentheses) *)
-let balanced_parentheses : grammar = todo
-
+let balanced_parentheses : grammar = 
+  {
+    symbols = [ S ];
+    terminals = [ '('; ')'; '['; ']'; '{'; '}'];
+    productions =
+      [              (* Indexes *)
+        S --> "S(S)S"; (* 0 *)
+        S --> "S[S]S"; (* 1 *)
+        S --> "S{S}S"; (* 2 *)
+        S --> "";    (* 3 *)
+      ];
+    start = S;
+  }
 
 (* #### Exercise 4, hard (same_amount)
 
    Hint: model the language of words where the number of 0's is
    one greater than the number of 1's and viceversa, then combine them.
 *)
-let same_amount : grammar = todo
+let same_amount : grammar = {
+  symbols = [ S ];
+  terminals = [ '0'; '1' ];
+  productions =
+    [              (* Indexes *)
+      S --> "S10S"; (* 0 *)
+      S --> "S01S"; (* 1 *)
+      S --> "0S1";  (* 2 *)
+      S --> "1S0";  (* 3 *)
+      S --> "";     (* 4 *)
+    ];
+  start = S;
+}
